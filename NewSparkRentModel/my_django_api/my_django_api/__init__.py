a4233eec5ff6ac8da6b953f6fa708caefa80e036
limeng <limeng@huizhaofang.com>
2018-08-04 00:54:10 +0800

    delete my_django_api

--- NewSparkRentModel/my_django_api/my_django_api/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-MODEL_PATH = ['/root/my_django_api/my_django_api/utils_apt/preprocess_models/model_ALL_ganji_beijing_gbdt20',
-                  '/root/my_django_api/my_django_api/utils_apt/preprocess_models/model_ALL_fangtianxia_beijing_gbdt20',
-                  '/root/my_django_api/my_django_api/utils_apt/preprocess_models/model_ALL_58_beijing_baifenzhi20_rf19']
--- NewSparkRentModel/my_django_api/my_django_api/utils_apt/spark_rentmodel_interface/__init__.py
+++
@@ -1,8 +0,0 @@
-#!usr/bin/ python
-#- * - coding:utf-8 - * -
-
-"""
-@author:limeng
-@file: __init__.py.py
-@time: 2018/07/{DAY}
-"""
